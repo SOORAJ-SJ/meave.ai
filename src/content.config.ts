8754ec24c7b11621ba9bb5dd986e65b782a135d2
SOORAJ-SJ <soorajmappidachery@gmail.com>
2025-10-13 11:38:57 +0530

    feat: initial commit (project setup)

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,14 +5,18 @@ const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
+	schema: () =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			heroImage: image().optional(),
+			tags: z.array(z.string()).optional(),
+			cardBackgroundColor: z.string(),
+			cardBaseFontColor : z.string(),
+			cardHoverFontColor : z.string(),
+			// Make sure the hero image is a valid URL
 		}),
 });
 
